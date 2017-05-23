a029ea68bb1bfc7b84a5975f5a0d50d4ead59834
amanz360 <amanz360@gmail.com>
2017-05-23 15:53:55 -0400

    New Scene for Grid Testing
    Added a grid script and node script and another scene to test them. The
    grid will successfully detect which parts of the terrain are walkable
    and which are not and also which node the player (capsule) is located
    in.

---
+++ Garnet/Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node{
+
+	public bool walkable;
+	public Vector3 position;
+
+
+	public Node(bool _walkable, Vector3 _position)
+	{
+		walkable = _walkable;
+		position = _position;
+	}
+}
